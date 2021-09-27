2872f0778009598aa0b1981bc2296df76c1f2a7f
Bemuseed <calebmullan@hotmail.com>
2021-09-27 12:29:32 +0100

    Revert "Made non-interfacing methods private (appended "_)""
    This reverts commit f13a7af13b056616e8e2e01e6d64116fa9a11eb4.

--- board.py
+++ board.py
@@ -52,10 +52,10 @@ class Board:
             "b": "X",
             "w": "O"}
 
-    def _get_contents(self, crd):
+    def get_contents(self, crd):
         return self.grid[crd.row][crd.column]
 
-    def _set_contents(self, crd, value):
+    def set_contents(self, crd, value):
         self.grid[crd.row][crd.column] = value
 
     """Outputs an ASCII representation of the grid to the console"""
@@ -70,22 +70,22 @@ class Board:
         output += "   A B C D E F G H J K L M N O P Q R S T   \n"
         return output
 
-    def _other_player(self, player):
+    def other_player(self, player):
         if player == "b":
             return "w"
         else:
             return "b"
 
-    def _is_valid_move(self, coord):
+    def is_valid_move(self, coord):
         if (0 <= coord.row <= 18) and (0 <= coord.row <= 18):
-            if self._get_contents(coord) is None:
+            if self.get_contents(coord) is None:
                 return True
             else:
                 return "Stones cannot be placed on other stones"
         else:
             return "Coordinates must be within the grid"
 
-    def _get_neighbours(self, crd):
+    def get_neighbours(self, crd):
         r = crd.row
         c = crd.column
 
@@ -100,17 +100,17 @@ class Board:
                 valid_neighbours += [neighbours[i]]
         return valid_neighbours
 
-    def _get_chain(self, coord):
+    def get_chain(self, coord):
         chain = [coord]
         current = coord
         to_check = []
-        colour = self._get_contents(coord)
+        colour = self.get_contents(coord)
 
         done = False
         while not done:
-            neighbours = self._get_neighbours(current)
+            neighbours = self.get_neighbours(current)
             for i in neighbours:
-                if self._get_contents(i) == colour:
+                if self.get_contents(i) == colour:
                     if not (i in chain):
                         to_check += [i]
                         chain += [i]
@@ -121,37 +121,37 @@ class Board:
                 del to_check[0]
         return chain
 
-    def _has_liberties(self, chain):
+    def has_liberties(self, chain):
         for stone in chain:
-            neighbours = self._get_neighbours(stone)
+            neighbours = self.get_neighbours(stone)
             for n in neighbours:
-                if self._get_contents(n) is None:
+                if self.get_contents(n) is None:
                     return True
         return False
 
-    def _capture(self, chain):
+    def capture(self, chain):
         for stone in chain:
-            self._set_contents(stone, None)
+            self.set_contents(stone, None)
 
-    def _capture_if_without_liberties(self, crd):
-        if self._get_contents(crd) is not None:
-            chain = self._get_chain(crd)
-            if not (self._has_liberties(chain)):
-                self._capture(chain)
+    def capture_if_without_liberties(self, crd):
+        if self.get_contents(crd) is not None:
+            chain = self.get_chain(crd)
+            if not (self.has_liberties(chain)):
+                self.capture(chain)
 
-    def _place_stone(self, placed, colour):
-        self._set_contents(placed, colour)
-        affected_stones = self._get_neighbours(placed)
+    def place_stone(self, placed, colour):
+        self.set_contents(placed, colour)
+        affected_stones = self.get_neighbours(placed)
         affected_stones += [placed]
         for s in affected_stones:
-            self._capture_if_without_liberties(s)
+            self.capture_if_without_liberties(s)
 
-    def _next_turn(self):
-        self.turn = self._other_player(self.turn)
+    def next_turn(self):
+        self.turn = self.other_player(self.turn)
 
     def make_move(self, move_coord):
-        self._place_stone(move_coord, self.turn)
-        self._next_turn()
+        self.place_stone(move_coord, self.turn)
+        self.next_turn()
 
     def pass_turn(self):
-        self._next_turn()
+        self.next_turn()
